87bcf05ae3b834ef9e666242339ebccec612cef3
AlexandreBjLiccardi <31627688+AlexandreBjLiccardi@users.noreply.github.com>
2022-07-05 11:26:10 +0200

    Update Main.java

--- main/java/launcher/Main.java
+++ main/java/launcher/Main.java
@@ -3,7 +3,8 @@ package launcher;
 public class Main {
 // Outil générique de lancement
 	public static void main(String[] args) throws Exception {
-			if(args.length >= 3)(new XqueryTest2()).process_i(args[0],args[1],args[2]);
+			if(args.length >= 3)(new XqueryTest2()).process_i(args[0],args[1],args[2],args);
+			else (new XqueryTest2()).process_i(args[0],args[1],args[2],args);
 	}
 }
 
